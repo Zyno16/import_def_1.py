654f52c51f1d6104f4ae6ebeca5be815a6bdc30f
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-12-06 18:02:13 +0000

    Add files via upload

---
+++ import_def_1.py
@@ -0,0 +1,17 @@
+
+from mydef import calc
+from mydef import hiall
+print("===========")
+calc(7,3,"+")
+calc(7,3,"-")
+calc(7,3,"*")
+calc(7,3,"/")
+calc(7,0,"/")
+print("******************")
+
+hiall("hasan","ehab","amr","amine")
+
+print("++++++++++++++++++++")
+
+
+hiall("hasan","ehab","amr","amine")
